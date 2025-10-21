dafffbd215d77aa0393f24dc4f6ce15a20cfc943
mb-raghu-34-3 <raghunandan.yadav.34.3@mountblue.tech>
2025-10-22 01:20:09 +0530

    configure supabase client

---
+++ supabase.js
@@ -0,0 +1,10 @@
+// Using environment variables to initialize Supabase client
+
+const {createClient} = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports=supabase;
