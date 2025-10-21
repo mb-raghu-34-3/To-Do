edb33222061e6a09b431f972362bbb0749800903
mb-raghu-34-3 <raghunandan.yadav.34.3@mountblue.tech>
2025-10-22 01:14:23 +0530

    use redux state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import HomePage from './components/HomePage'
+import ToDoApp from './components/ToDo';
+
 function App() {
- return(<>ToDo</>)
+  const auth = useSelector(state => state.auth)  
+  const [user,setUser] = useState(false)
+
+  useEffect(() => {
+    if(auth.name){
+      setUser(true)
+    }else{
+      setUser(false)
+    }
+  }, [auth]);
+
+  if(user){
+    return (
+      <ToDoApp/>
+    );
+  }else{
+    return(
+     <HomePage/>
+    )
+  }
 }
 
 export default App;
